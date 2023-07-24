628d31e55c5945d28189d59bb1cfb988e82e3ef5
iman <imaniman1377@yahoo.com>
2023-07-25 00:33:35 +0330

    [chore:resume]: add swagger to application

--- apps/resume/src/main.ts
+++ apps/resume/src/main.ts
@@ -4,25 +4,22 @@ import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { AppConfigs } from './common/constant/app-config.constant';
 import { Logger } from '@nestjs/common';
+import { setupSwagger } from './common/helper/function/setup-swagger.helper';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap(): Promise<void> {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   const configService = app.get<ConfigService>(ConfigService);
 
-  const GLOBAL_PREFIX = 'api';
-
-  app.setGlobalPrefix(GLOBAL_PREFIX);
-
   const port = configService.get(AppConfigs.APP);
 
-  await app.listen(port);
+  setupSwagger(app);
 
+  await app.listen(port);
+  Logger.log(`🚀 Application is running on: http://localhost:${port}`);
   Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${GLOBAL_PREFIX}`
-  );
-  Logger.log(
-    `🚀APIs documents is running on: http://localhost:${port}/${GLOBAL_PREFIX}/doc`
+    `🚀 Application documents is running on: http://localhost:${port}/docs`
   );
 }
 
