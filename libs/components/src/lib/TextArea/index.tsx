a1dd8f9f732d2704228007995f924fa822498421
amir <amir.developer8@gmail.com>
2023-08-11 22:28:00 +0330

    fix(text field and text area): fix iconcd '/home/amir/Documents/bright/bright-resume'

--- libs/components/src/lib/TextArea/index.tsx
+++ libs/components/src/lib/TextArea/index.tsx
@@ -1,15 +1,13 @@
 import { FC } from "react";
 import cls from "classnames";
-import Image from "next/image";
-// svg
-import SolarPenBoldSvg from "@bright-resume/assets/svg/solar-pen-bold.svg";
 // types
 import { TypographyVariant } from "../types/index.type";
 // components
+import Typography from "../Typography";
+import { SolarPenBoldIcon } from "../Icons";
 // locals
 import { useData } from "./index.hook";
 import classes from "./index.module.scss";
-import Typography from "../Typography";
 
 type TextAreaProps = React.TextareaHTMLAttributes<HTMLTextAreaElement> & {
   variant?: TypographyVariant;
@@ -20,13 +18,9 @@ export const TextArea: FC<TextAreaProps> = ({ variant = "h6", ...props }) => {
 
   const renderIcon = () => {
     return (
-      <Image
-        className={classes.edit__icon}
-        src={SolarPenBoldSvg}
-        alt="solar-pen"
-        width={18}
-        height={18}
-      />
+      <div className={classes.edit__icon}>
+        <SolarPenBoldIcon width={18} height={18} />
+      </div>
     );
   };
 
@@ -36,7 +30,9 @@ export const TextArea: FC<TextAreaProps> = ({ variant = "h6", ...props }) => {
         component="textarea"
         onFocus={data.handleActiveInput}
         autoFocus={data.isInputActive}
-        className={classes.input}
+        className={cls(classes.input, {
+          [classes.disable__input]: !data.isInputActive,
+        })}
         onKeyDown={(event) =>
           event.key === "Escape" && data.handleDeActiveInput()
         }
