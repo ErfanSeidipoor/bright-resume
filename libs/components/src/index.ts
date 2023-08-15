38c3b4880184409ccea3291c26b6b0a36bf9c13b
Basir <basirheydari456@gmail.com>
2023-08-15 19:30:05 +0330

    feat (typography test): add test #77

--- libs/components/src/index.ts
+++ libs/components/src/index.ts
@@ -1,5 +1,4 @@
 export * from "./lib/AboutMe";
 export * from "./lib/Button";
-export * from "./lib/components";
 export * from "./lib/Icons";
 export * from "./lib/Typography";
--- libs/components/src/lib/Typography/index.stories.tsx
+++ libs/components/src/lib/Typography/index.stories.tsx
@@ -1,9 +1,9 @@
-import { ComponentMeta, ComponentStory } from '@storybook/react';
-import Typography from '.';
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import Typography from ".";
 
 export default {
   component: Typography,
-  title: 'Typography',
+  title: "Typography",
 } as ComponentMeta<typeof Typography>;
 
 const Template: ComponentStory<typeof Typography> = (args) => {
@@ -11,28 +11,28 @@ const Template: ComponentStory<typeof Typography> = (args) => {
 };
 
 export const H1 = Template.bind({});
-H1.args = { variant: 'h1', children: 'H1' };
+H1.args = { variant: "h1", children: "H1" };
 
 export const H2 = Template.bind({});
-H2.args = { variant: 'h2', children: 'H2' };
+H2.args = { variant: "h2", children: "H2" };
 
 export const H3 = Template.bind({});
-H3.args = { variant: 'h3', children: 'H3' };
+H3.args = { variant: "h3", children: "H3" };
 
 export const H4 = Template.bind({});
-H4.args = { variant: 'h4', children: 'H4' };
+H4.args = { variant: "h4", children: "H4" };
 
 export const H5 = Template.bind({});
-H5.args = { variant: 'h5', children: 'H5' };
+H5.args = { variant: "h5", children: "H5" };
 
 export const H6 = Template.bind({});
-H6.args = { variant: 'h6', children: 'H6' };
+H6.args = { variant: "h6", children: "H6" };
 
 export const H7 = Template.bind({});
-H7.args = { variant: 'h7', children: 'H7' };
+H7.args = { variant: "h7", children: "H7" };
 
 export const H8 = Template.bind({});
-H8.args = { variant: 'h8', children: 'H8' };
+H8.args = { variant: "h8", children: "H8" };
 
 export const H9 = Template.bind({});
-H9.args = { variant: 'h9', children: 'H9' };
+H9.args = { variant: "h9", children: "H9" };
