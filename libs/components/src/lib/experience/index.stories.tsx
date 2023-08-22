7e9460b2aeddd2678f6a54fcc7228822139a3f8e
amirmohammad-mohamadi <amir.developer8@gmail.com>
2023-08-22 21:15:33 +0330

    feat(experience component): add functionality and ui-ux

--- libs/components/src/lib/experience/index.stories.tsx
+++ libs/components/src/lib/experience/index.stories.tsx
@@ -1,5 +1,7 @@
 import { ComponentMeta, ComponentStory } from "@storybook/react";
+import { useState } from "react";
 import Experience from ".";
+import { ExperienceChildProps } from "../types/index.type";
 
 export default {
   component: Experience,
@@ -7,9 +9,34 @@ export default {
 } as ComponentMeta<typeof Experience>;
 
 const Template: ComponentStory<typeof Experience> = (args) => {
+  const defaultItems: ExperienceChildProps = {
+    id: "child-1",
+    position: { placeholder: "Position" },
+    company: { placeholder: "company name" },
+    description: {
+      placeholder:
+        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    },
+  };
+  const [items, setItems] = useState<ExperienceChildProps[]>([defaultItems]);
+  const onIncrease = () =>
+    setItems((prevState) => [
+      ...prevState,
+      { ...defaultItems, id: `child-${prevState.length + 1}` },
+    ]);
+  const onDecrease = (id: string) => {
+    const newItems = items.filter((item) => item.id !== id);
+    setItems(newItems);
+  };
+
   return (
-    <div id="theme-purple">
-      <Experience {...args} />
+    <div id="theme-blue">
+      <Experience
+        {...args}
+        items={items}
+        onIncrease={onIncrease}
+        onDecrease={onDecrease}
+      />
     </div>
   );
 };
