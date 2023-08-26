08d1c01ee8d639b0bbd58c4035608d44e2d80bca
adib mokhtari <adib.mokhtari@gmail.com>
2023-08-27 02:14:17 +0330

    feat[component-about-me]: - update AboutMe component - update AboutMe test

--- libs/components/src/lib/AboutMe/index.spec.tsx
+++ libs/components/src/lib/AboutMe/index.spec.tsx
@@ -1,52 +1,59 @@
 import "@testing-library/jest-dom/extend-expect";
 
+import { faker } from "@faker-js/faker";
 import { render, screen } from "@testing-library/react";
 import user from "@testing-library/user-event";
 
 import AboutMe from "./";
 
-const onChangeHeaderValue = jest.fn();
-const onChangeBodyValue = jest.fn();
+const mockedOnChangeHeader = jest.fn();
+const mockedOnChangeDescription = jest.fn();
+
+const renderComponent = () => {
+  const { baseElement } = render(
+    <AboutMe
+      header={{
+        placeholder: "About Me",
+        onChange: mockedOnChangeHeader,
+      }}
+      description={{
+        label: "About me description",
+        onChange: mockedOnChangeDescription,
+      }}
+    />
+  );
+
+  return { baseElement };
+};
 
 describe("AboutMe Component", () => {
-  const renderComponent = (headerValue: string, bodyValue: string) => {
-    render(
-      <AboutMe
-        headerValue={headerValue}
-        onChangeHeaderValue={onChangeHeaderValue}
-        bodyValue={bodyValue}
-        onChangeBodyValue={onChangeBodyValue}
-      />
-    );
-  };
+  it("should render successfully", () => {
+    const { baseElement } = renderComponent();
+    const title = screen.getByRole("heading", { level: 2 });
+    const descriprion = screen.getByRole("textbox");
+    expect(title).toBeDefined();
+    expect(descriprion).toBeDefined();
+    expect(baseElement).toBeTruthy();
+  });
 
-  test("renders header and body with provided values, then shows input element when user click on each header element, then shows header element when user blured from each input element", async () => {
-    const headerValue = "About Me";
-    const bodyValue =
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+  it("shows input after user clicked", async () => {
+    renderComponent();
+    const title = screen.getByRole("heading", { level: 2 });
+    user.click(title);
+    const input = await screen.findByPlaceholderText("About Me");
+    expect(input).toBeDefined();
+  });
 
-    renderComponent(headerValue, bodyValue);
+  it("shows input value after user type something", async () => {
+    const TEXT = faker.word.noun();
+    renderComponent();
+    const title = screen.getByRole("heading", { level: 2 });
+    user.click(title);
 
-    const headerElement = screen.getByRole("heading", { level: 1 });
-    expect(headerElement.textContent).toBe(headerValue);
-
-    const bodyElement = screen.getByRole("heading", { level: 6 });
-    expect(bodyElement.textContent).toBe(bodyValue);
-
-    await user.click(headerElement);
-    const headerInput = await screen.findByRole("textbox");
-
-    expect(headerInput).toBeDefined();
-    await headerInput.blur();
-
-    expect(headerElement).toBeDefined();
-
-    await user.click(bodyElement);
-    const bodyInput = await screen.findByRole("textbox");
-
-    expect(bodyInput).toBeDefined();
-    await bodyInput.blur();
-
-    expect(bodyElement).toBeDefined();
+    const input = await screen.findByPlaceholderText("About Me");
+    await user.keyboard(TEXT);
+    expect(input).toBeDefined();
+    expect(mockedOnChangeHeader).toHaveBeenCalled();
+    expect(mockedOnChangeHeader).toHaveBeenCalledTimes(TEXT.length);
   });
 });
