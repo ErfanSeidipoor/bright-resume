b16e5fd826022529190103a3ea53c4360054cb46
iman <imaniman1377@yahoo.com>
2023-09-04 18:49:10 +0330

    fix (components/typography):
    fix error in spec by using forwardRef

--- libs/components/src/lib/Typography/index.tsx
+++ libs/components/src/lib/Typography/index.tsx
@@ -3,6 +3,7 @@ import cls from "classnames";
 import { TypographyVariant } from "../types/index.type";
 
 import classes from "./index.module.scss";
+import { forwardRef } from "react";
 
 export type TypographyProps<T extends React.ElementType> = {
   component?: T;
@@ -11,39 +12,44 @@ export type TypographyProps<T extends React.ElementType> = {
   className?: string;
 } & React.ComponentPropsWithoutRef<T>;
 
-export const Typography = <T extends React.ElementType = "p">({
-  variant = "h7",
-  component,
-  children,
-  className,
-  ...props
-}: TypographyProps<T>) => {
-  const getComponent = (): React.ElementType => {
-    if (!component && ["h7", "h8", "h9"].includes(variant)) {
-      return "p";
-    }
-    return component || (variant as React.ElementType);
-  };
+export const Typography = forwardRef(
+  <T extends React.ElementType = "p">(
+    {
+      variant = "h7",
+      component,
+      children,
+      className,
+      ...props
+    }: TypographyProps<T>,
+    ref: React.ForwardedRef<T>
+  ) => {
+    const getComponent = (): React.ElementType => {
+      if (!component && ["h7", "h8", "h9"].includes(variant)) {
+        return "p";
+      }
+      return component || (variant as React.ElementType);
+    };
 
-  const Component = getComponent();
+    const Component = getComponent();
 
-  const rootClasses = cls(classes.root, className, {
-    [classes.h1]: variant === "h1",
-    [classes.h2]: variant === "h2",
-    [classes.h3]: variant === "h3",
-    [classes.h4]: variant === "h4",
-    [classes.h5]: variant === "h5",
-    [classes.h6]: variant === "h6",
-    [classes.h7]: variant === "h7",
-    [classes.h8]: variant === "h8",
-    [classes.h9]: variant === "h9",
-  });
+    const rootClasses = cls(classes.root, className, {
+      [classes.h1]: variant === "h1",
+      [classes.h2]: variant === "h2",
+      [classes.h3]: variant === "h3",
+      [classes.h4]: variant === "h4",
+      [classes.h5]: variant === "h5",
+      [classes.h6]: variant === "h6",
+      [classes.h7]: variant === "h7",
+      [classes.h8]: variant === "h8",
+      [classes.h9]: variant === "h9",
+    });
 
-  return (
-    <Component className={rootClasses} {...props}>
-      {children}
-    </Component>
-  );
-};
+    return (
+      <Component className={rootClasses} {...props} ref={ref}>
+        {children}
+      </Component>
+    );
+  }
+);
 
 export default Typography;
