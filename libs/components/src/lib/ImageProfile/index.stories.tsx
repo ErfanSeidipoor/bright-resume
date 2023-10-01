3c58e61d822f10a7fa97786529332adf2ba898d0
adib mokhtari <adib.mokhtari@gmail.com>
2023-10-02 00:54:54 +0330

    76-image-profile-uploader: implement image profile uploader - update palette - add profileImage icon - implement component, logic, ui

---
+++ libs/components/src/lib/ImageProfile/index.stories.tsx
@@ -0,0 +1,22 @@
+import {
+  Meta,
+  StoryFn,
+} from "@storybook/react";
+
+import ImageProfile from "./";
+
+export default {
+  component: ImageProfile,
+  title: "ImageProfile",
+} as Meta<typeof ImageProfile>;
+
+const Template: StoryFn<typeof ImageProfile> = () => {
+  return (
+    <div id="theme-blue">
+      <ImageProfile />
+    </div>
+  );
+};
+
+export const Main = Template.bind({});
+Main.args = {};
