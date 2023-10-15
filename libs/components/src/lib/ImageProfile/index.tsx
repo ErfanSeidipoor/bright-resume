f6324ee1d0035e12bbf877cc6aa41c5241ec7e75
adib mokhtari <adib.mokhtari@gmail.com>
2023-10-16 01:04:17 +0330

    image-profile-uploader-test: - add test for image-profile - update component, styles, logic

--- libs/components/src/lib/ImageProfile/index.tsx
+++ libs/components/src/lib/ImageProfile/index.tsx
@@ -7,21 +7,35 @@ import { texts } from "./text";
 export const ImageProfile = () => {
   const { inputRef, profileImage, handleChange, uploadHandler } = useData();
 
+  const renderImage = () => {
+    if (!profileImage) {
+      return (
+        <>
+          <ProfileImageIcon />
+          <Typography variant="h6" rootClassName={classes.text__wrapper}>
+            {texts.yourPicture}
+          </Typography>
+        </>
+      );
+    } else {
+      return <img src="" alt="profile-img" />;
+    }
+  };
+
   return (
-    <div className={classes.container} onClick={uploadHandler}>
+    <div
+      className={classes.container}
+      data-testid="image-uploader-container"
+      onClick={uploadHandler}
+    >
       <input
+        data-testid="image-file-input"
         ref={inputRef}
         type="file"
         onChange={handleChange}
-        accept=".png,.jpeg"
+        accept="image/*"
       />
-      <ProfileImageIcon />
-      <Typography variant="h6" rootClassName={classes.text__wrapper}>
-        {texts.yourPicture}
-      </Typography>
-      {profileImage && <img src={profileImage} alt="profile-img" />}
+      {renderImage()}
     </div>
   );
 };
-
-export default ImageProfile;
