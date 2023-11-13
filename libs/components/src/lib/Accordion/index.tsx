1bb0ba829493fb1fd40117540037036830ff4b59
ali.shirzad <ali.shirzad@ponisha.com>
2023-11-13 23:04:35 +0330

    WIP: fix accordion

--- libs/components/src/lib/Accordion/index.tsx
+++ libs/components/src/lib/Accordion/index.tsx
@@ -1,5 +1,6 @@
+"use client";
 import cls from "classnames";
-import { FC, ReactNode } from "react";
+import { FC, ReactNode, useEffect } from "react";
 import { PlusIcon } from "../Icons/plus";
 import classes from "./index.module.scss";
 import { useData } from "./index.hook";
@@ -10,10 +11,47 @@ interface AccordionItemProps {
 }
 
 export const AccordionItem: FC<AccordionItemProps> = ({ title, children }) => {
-  const {isOpen, handleToggle} = useData();
+  const { isOpen, handleToggle } = useData();
+  const slug = () => title.toLowerCase().replace(/ /g, "-");
+
+  useEffect(() => {
+    const accordionItems = document.querySelectorAll(
+      `.${classes.accordion__item}`
+    );
+
+    accordionItems.forEach((item) => {
+      item.addEventListener("click", () => {
+        new Promise((resolve) => {
+          setTimeout(() => {
+            const content = item.querySelector<HTMLElement>(
+              `.${classes.accordion__item__children}`
+            );
+            if (!content) return;
+            if (
+              content.classList.contains(
+                classes.accordion__item__children__open
+              )
+            ) {
+              // If the accordion is open, close it
+              content.style.height = `${content.scrollHeight}px`;
+            } else {
+              // If the accordion is closed, open it
+              content.style.height = "1px";
+            }
+            item.classList.toggle("__open");
+            resolve(null);
+          }, 0);
+        });
+      });
+    });
+  }, []);
 
   return (
-    <div className={classes.accordion__item} role="button" aria-expanded={isOpen}>
+    <div
+      className={classes.accordion__item}
+      role="button"
+      aria-expanded={isOpen}
+    >
       <div
         className={cls(classes.accordion__item__header, {
           [classes.accordion__item__header__open]: isOpen,
@@ -36,7 +74,7 @@ export const AccordionItem: FC<AccordionItemProps> = ({ title, children }) => {
           className={cls(classes.accordion__item__header__icon, {
             [classes.accordion__item__header__icon__open]: isOpen,
           })}
-          id={`accordion-item-${title}`}
+          id={`accordion-item-${slug}`}
         >
           <PlusIcon />
         </div>
@@ -68,4 +106,3 @@ export const Accordion: FC<AccordionProps> = ({ items }) => {
     </div>
   );
 };
-
