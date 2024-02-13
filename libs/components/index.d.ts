685d0ceb5f329aa59a37a144e20de60832411282
Amir <amir.developer8@gmail.com>
2024-02-13 12:39:08 +0330

    fix(components): change bundler to webpack

--- libs/components/index.d.ts
+++
@@ -1,8 +0,0 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-declare module '*.svg' {
-  const content: any;
-  export const ReactComponent: React.FunctionComponent<
-    React.SVGProps<SVGSVGElement> & { title?: string }
-  >;
-  export default content;
-}
