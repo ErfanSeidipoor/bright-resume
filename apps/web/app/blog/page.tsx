bc576294b86f6535911947f1fa2e0c37373b5a2f
mohammad <aahrari5@gmail.com>
2024-02-25 10:51:40 +0330

    integrate blog page

--- apps/web/app/blog/page.tsx
+++ apps/web/app/blog/page.tsx
@@ -1,23 +1,42 @@
 import { BlogContent, BlogTitle } from "@web/components/blog";
+import queryString, { StringifiableRecord } from "query-string";
 import classes from "./page.module.scss";
 
-type BlogPageProps = {};
+interface IQuery {
+  search: string;
+  "pagination[page]": string;
+}
 
-const getPosts = async () => {
+type BlogPageProps = {
+  searchParams?: IQuery;
+};
+
+const getPosts = async ({ query }: { query: StringifiableRecord }) => {
   const revalidateTime = 60 * 60 * 3;
-  const response = await fetch(`${process.env.REACT_APP_SERVER_URL}/posts`, {
-    next: { revalidate: revalidateTime },
-  });
+  const response = await fetch(
+    queryString.stringifyUrl({
+      url: `${process.env.REACT_APP_SERVER_URL}/posts`,
+      query: query,
+    }),
+    {
+      next: { revalidate: revalidateTime },
+    }
+  );
   return response.json();
 };
 
 export default async function Index(props: BlogPageProps) {
-  const posts = await getPosts();
+  const { searchParams } = props;
+  const convertQueryFromSearchParams: Record<keyof IQuery, string> = {
+    search: searchParams?.search || "",
+    "pagination[page]": searchParams?.["pagination[page]"] || "1",
+  };
+  const posts = await getPosts({ query: convertQueryFromSearchParams });
 
   return (
     <div className={classes.container}>
       <BlogTitle />
-      <BlogContent blogsData={posts.data} />
+      <BlogContent blogsData={posts} />
     </div>
   );
 }
--- apps/web/components/blog/content/index.tsx
+++ apps/web/components/blog/content/index.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { FC } from "react";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { BlogCard, NavigationButton } from "@bright-resume/components";
 
 import classes from "./index.module.scss";
@@ -9,9 +10,14 @@ type BlogContentProps = {
 };
 
 const BlogContent: FC<BlogContentProps> = ({ blogsData }) => {
+  const searchParams = useSearchParams();
+  const path = usePathname();
+  const router = useRouter();
+  const currentPage = Number(searchParams?.get("page")) || 1;
+
   const renderBlogContent = () => {
-    if (blogsData.length <= 0) return;
-    return blogsData.map((blog, index) => (
+    if (blogsData.data.length <= 0) return;
+    return blogsData.data.map((blog, index) => (
       <div className={classes.blog__card__wrapper} key={blog.id}>
         <BlogCard
           rootClassName={classes.blog__card}
@@ -29,8 +35,32 @@ const BlogContent: FC<BlogContentProps> = ({ blogsData }) => {
     ));
   };
 
+  const handleNextPage = (page: number) => {
+    const urlPagination = new URLSearchParams(searchParams);
+    urlPagination.set("page", (page + 1).toString());
+    router.replace(`${path}?${urlPagination.toString()}`);
+  };
+
+  const handlePrevPage = (page: number) => {
+    const urlPagination = new URLSearchParams(searchParams);
+    urlPagination.set("page", (page - 1).toString());
+    if (page <= 2) {
+      urlPagination.delete("page");
+    }
+    router.replace(`${path}?${urlPagination.toString()}`);
+  };
+
   const renderNavigationButton = () => {
-    return <NavigationButton className="navigation-button" color="grey" />;
+    return (
+      <NavigationButton
+        className="navigation-button"
+        color="grey"
+        onNextPage={() => handleNextPage(currentPage)}
+        onPrevPage={() => handlePrevPage(currentPage)}
+        hasNextPage={currentPage >= blogsData?.meta?.pagination.pageCount}
+        hasPrevPage={currentPage <= 1}
+      />
+    );
   };
 
   return (
